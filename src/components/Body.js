3f05211ef32678d5f8879708051b4e66942b46bc
Vemula Mounika <vemulamounika540@gmail.com>
2026-01-22 16:29:04 +0530

    Implement filtering for top-rated restaurants
    Added state management for restaurant filtering.

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,25 @@
 import restaurantList from "../utils/mockData";
-
+import { useState } from "react";
 import RestaurantCard from "./RestaurantCard";
 
 const Body = () => {
+ const [listOfRestaurants, setListOfRestaurants] = useState(restaurantList)
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="search">
+      <button 
+       onClick={()=>{
+  const filteredList = listOfRestaurants.filter(
+        (res)=>res.info.avgRating > 4)
+       setListOfRestaurants(filteredList)
+       console.log(filteredList)
+       }}
+       >
+        Top Rated Restarantrs
+      </button>
+        </div>
       <div className="res-container">
-        {restaurantList.map((restaurant) => (
+       {listOfRestaurants.map((restaurant) => (
           <RestaurantCard
             key={restaurant.info.id}
             restaurantData={restaurant}
